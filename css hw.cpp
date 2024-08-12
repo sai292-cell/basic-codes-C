bc0550fdd669ffbe67b0c1fc2ccad94f043e51ea
Atharva1811 <atharvamilinddeshmukh@gmail.com>
2024-08-12 22:45:50 +0530

    Add files via upload

---
+++ css hw.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,c;
+	cout<<"enter your no";
+	cin>>a;
+	cout<<"enter your no";
+	cin>>b;
+	c=2*a*b;
+	cout<<"perimeter of the rectangle is="<<c;
+	return 0;
+}
